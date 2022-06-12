3e6237dcc41728966567b414ac924f41cc93881a
pklemmer <103751506+pklemmer@users.noreply.github.com>
2022-06-12 14:48:55 +0200

    Heatmap is now generated via ggplot2 (previously base), added function to save heatmap as svg

--- rot-heatmap.R
+++ rot-heatmap.R
@@ -9,26 +9,31 @@ xp2 <- read_delim("expression/8_dmso_vs_8_rot50_24.tsv",
 xp3 <- read_delim("expression/8_dmso_vs_8_rot100_24.tsv", 
                   delim = "\t", escape_double = FALSE, 
                   trim_ws = TRUE)
-xp4 <- read_delim("expression/15_dmso_vs_15_rot50_12.tsv", 
-                  delim = "\t", escape_double = FALSE, 
-                  trim_ws = TRUE)
-xp5<- read_delim("expression/15_dmso_vs_15_rot50_24.tsv", 
-                 delim = "\t", escape_double = FALSE, 
-                 trim_ws = TRUE)
-xp6 <- read_delim("expression/15_dmso_vs_15_rot100_24.tsv", 
-                  delim = "\t", escape_double = FALSE, 
-                  trim_ws = TRUE)
-  #Loading GEO2R data sets
+  #Loading expression datasets 
 
+install.packages("reshape")
 install.packages("gdata")
+library(reshape)
 library(gdata)
-  #gdata has the cbindX function, allowing cbind with differing number of rows (rows are extended and filled with NA)
+#gdata has the cbindX function, allowing cbind with differing number of rows (rows are extended and filled with NA)
 df <- cbindX( xp1[,"GENE_SYMBOL"],xp1[,"logFC"], xp2[,"logFC"], xp3[,"logFC"])
-colnames(df) <- c("Gene symbol", "xp1", "xp2", "xp3")
-  #Creating a df containing gene symbols and the logFC of all samples
-
+colnames(df) <- c("Genesymbol", "xp1", "xp2", "xp3")
+#Creating a df containing gene symbols and the logFC of all samples
 rownames(df) <- make.names(df[,1], unique = TRUE)
 xpmatrix <- as.matrix(df[2:4])
-  #Heatmap functions require matrix input, the previous 2 lines convert the df to a matrix with the gene names as row names
-heatmap(xpmatrix, Rowv=NA, Colv=NA)
+df<-melt(xpmatrix)
+colnames(df) <- c("Gene","dataset","logFC")
+  #Making a dataframe that can be used with ggplot2
+library(ggplot2)
+rotheatmap <- ggplot(df, aes(x = dataset, y = Gene, fill = logFC)) +
+  geom_tile()+
+  scale_fill_gradient2(low = "#0000ff",
+                       mid = "#ffffff",
+                       high = "#ff0000",
+                       limits=c(-0.26,0.26),
+                       breaks=seq(-0.26,0.26,by=0.13))
+rotheatmap=rotheatmap
+install.packages("svglite")
+library(svglite)
+ggsave(file="rotheatmap.svg", plot=rotheatmap, width=10,height=10)
 
